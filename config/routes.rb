57c4cb0c56e3198fcc7862cf75cc81192420cbb9
Justine B <justinegayton@gmail.com>
2019-02-01 20:54:22 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
   #resources :products
+=======
+  resources :products 
+  root 'products#index'
+  
+  
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+>>>>>>> b5b9ce0061afec4d5cbf9b9fc0696a20a5f44dcd
 
   post '/' => 'products#add_to_cart', as: "product_cart"
 
